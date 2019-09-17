305113f5268ee3ae2f98d74c4492dd94de5d8f6d
tracer-pa-public <tracer@njuics.org>
2019-09-17 20:08:44 +0800

    >  nemu 181860021 fsq Linux debian 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  20:08:44 up  2:05,  1 user,  load average: 0.09, 0.04, 0.00 bad2ddc32e331982cb252f8e47b9e5da802f8fa3

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -36,7 +36,7 @@ void Set_OF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 		case 8:
 			result = sign_ext(result&0xFF,8);
 			src = sign_ext(src & 0xFF, 8);
-			dest = sign_ext(Dest & 0xFF,8);
+			dest = sign_ext(dest & 0xFF,8);
 			break;
 		case 16:
 			result = sign_ext(result&0xffff,16);
