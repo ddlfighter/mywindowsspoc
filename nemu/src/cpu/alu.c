dd165b699442ff333678d3570865ecce73d62ca2
tracer-pa-public <tracer@njuics.org>
2019-09-18 15:47:33 +0800

    >  nemu 181860021 fsq Linux debian 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  15:47:33 up  1:29,  1 user,  load average: 0.12, 0.17, 0.15 10880898fe6be4493007c4aa5e0f74c4540c9207

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -92,7 +92,7 @@ void Set_CF_sbb(uint32_t des,uint32_t src,size_t data_size){
 void Set_CF_shl(uint32_t dest,uint32_t src,size_t data_size){
 	dest = dest<<src;
 	dest = sign_ext(dest&(0xFFFFFFFF>>(32-data_size)),data_size);
-	uint32_t signdet = 0x1<<(data_size-src);
+	uint32_t signdet = 0x1<<(data_size-src+1);
 	cpu.eflags.CF = (dest&signdet)>0;
 }
 void Set_OF_sub(uint32_t src,uint32_t dest,uint32_t res,size_t data_size){
