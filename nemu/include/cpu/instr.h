d37899a5e62d2877b049c0442540b8a3ea1d9f12
tracer-pa-public <tracer@njuics.org>
2019-10-25 17:27:14 +0800

    >  nemu 181860021 fsq Linux debian 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:27:14 up 33 min,  1 user,  load average: 0.42, 0.18, 0.16 2a86d5849bd7e2be63b5ab6e1fd803b55309033

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -29,6 +29,7 @@ extern uint8_t data_size;
 #include "cpu/instr/sub.h"
 #include "cpu/instr/xor.h"
 #include "cpu/instr/cmp.h"
+#include "cpu/instr/push.h"
 /* TODO: add more instructions here */
 make_instr_func(inc_i2r_v);
 make_instr_func(OUT_a2);
