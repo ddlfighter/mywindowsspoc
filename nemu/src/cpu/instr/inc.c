54e60b846099300353bdf15722b1adc558c245d0
monitor <tracer@njuics.org>
2019-10-26 12:25:01 +0800

    git monitor commit.

---
+++ nemu/src/cpu/instr/inc.c
@@ -0,0 +1,12 @@
+#include "cpu/instr.h"
+
+static void instr_execute_1op() 
+{
+	operand_read(&opr_src);
+    int x = cpu.eflags.CF;
+	opr_src.val = alu_add(1, opr_src.val, opr_src.data_size);
+    cpu.eflags.CF = x;
+	operand_write(&opr_src);
+}
+
+make_instr_impl_1op(inc, rm, v)
