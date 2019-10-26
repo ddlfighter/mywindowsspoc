0ba31bcbb0627c5ac506d326064584d14f1252e3
monitor <tracer@njuics.org>
2019-10-26 17:11:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -42,7 +42,6 @@ make_instr_func(OUT_a2);
 make_instr_func(mov_i2a_b);
 make_instr_func(jmp_short_);
 make_instr_func(cmps_bv);
-make_instr_func(sbb_i2a_b);
 make_instr_func(push_es);
 make_instr_func(jnbe_short);
 make_instr_func(or_i2rm_b);
