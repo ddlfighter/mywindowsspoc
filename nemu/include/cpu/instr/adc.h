0287d4a66b6d58b5ead117925f3378e5e8a00d0b
monitor <tracer@njuics.org>
2019-10-26 17:10:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/adc.h
+++ nemu/include/cpu/instr/adc.h
@@ -3,6 +3,7 @@
 
 make_instr_func(adc_i2rm_bv);
 make_instr_func(adc_i2rm_v);
+make_instr_func(adc_i2a_b);
 
 
 #endif
--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -13,3 +13,4 @@ static void instr_execute_2op()
 
 make_instr_impl_2op(adc,i,rm,bv);
 make_instr_impl_2op(adc,i,rm,v);
+make_instr_impl_2op(adc,i,a,b);
