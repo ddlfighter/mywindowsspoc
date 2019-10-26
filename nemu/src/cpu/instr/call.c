a57ea192ae14ac4874a55296bef6bd6d8049c64d
monitor <tracer@njuics.org>
2019-10-26 12:56:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -27,27 +27,24 @@ make_instr_func(call_i_v){
 }
 make_instr_func(call_rm_v){
     int len = 1;
-    OPERAND rel;
-    rel.type = OPR_IMM;
-    rel.sreg = SREG_CS;
-    rel.data_size = data_size;
-    rel.addr = eip + 1;
+    OPERAND rm;
+    rm.data_size = data_size;
+    len += modrm_rm(eip +1, &rm);
 
-    operand_read(&rel);
+    operand_read(&rm);
 
-    int offset = sign_ext(rel.val,data_size);
-    print_asm_1("call","",1 + data_size/8,&rel);
+    int offset = sign_ext(rm.val,data_size);
 
     OPERAND temp;
     temp.type = OPR_MEM;
     temp.sreg = SREG_CS;
     temp.data_size = data_size;
     cpu.esp -= 4;
-    temp.val = eip + 1 + data_size/8;
+    temp.val = eip + len;
     temp.addr = cpu.esp;
     operand_write(&temp);
 
     cpu.eip += offset;
-    return 1 + data_size/8;
+    return len;
 
 }
