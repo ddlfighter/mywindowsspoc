e6fdec07eb0cbd2c8eaac1d1e7160863bec3e498
monitor <tracer@njuics.org>
2019-10-26 16:46:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jcc.c
+++ nemu/src/cpu/instr/jcc.c
@@ -25,6 +25,7 @@ static void instr_execute_1op_cc_fail() {/* Do nothing */}
 	make_instr_impl_1op_cc(j, i, suffix, no) \
 	make_instr_impl_1op_cc(j, i, suffix, np) \
 	make_instr_impl_1op_cc(j, i, suffix, ns) \
+	nake_instr_impl_1op_cc(j, i, suffix, nb) \
 
 jcc_helper(near)
 jcc_helper(short_)
