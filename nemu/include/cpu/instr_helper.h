87d0f19a2f8422ab3b1868ba0eb5b54094527936
tracer-pa-public <tracer@njuics.org>
2019-10-27 02:58:20 +0800

    >  clean 181860021 fsq Linux debian 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  02:58:20 up  3:29,  1 user,  load average: 0.10, 0.27, 0.22 65474d99b283a4542c810d414f78031364c54310

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -218,7 +218,7 @@ static inline bool inv_cc();
 	cpu.eflags.SF != cpu.eflags.OF
 
 #define condition_le \
-	cpu.eflags.ZF == 1 && cpu.eflags.SF!=cpu.eflags.OF
+	cpu.eflags.ZF == 1 || cpu.eflags.SF!=cpu.eflags.OF
 
 #define condition_ecxz \
 	cpu.ecx == 0
