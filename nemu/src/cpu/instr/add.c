36cd31062eca66decf79ac7a3a646d74d1cc41fb
monitor <tracer@njuics.org>
2019-10-26 14:18:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -15,4 +15,4 @@ make_instr_impl_2op(add,i,rm,bv);
 make_instr_impl_2op(add,r,rm,b);
 make_instr_impl_2op(add,r,rm,v);
 make_instr_impl_2op(add,i,r,b);
-make_instr_impl_2op(add,r,rm,v);
+make_instr_impl_2op(add,rm,r,v);
