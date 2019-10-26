cc17dcdb336acaa994e763f7ab5639f746a04d57
monitor <tracer@njuics.org>
2019-10-26 15:13:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/sub.h
+++ nemu/include/cpu/instr/sub.h
@@ -2,7 +2,7 @@
 #define __INSTR_SUB_H__
 
 make_instr_func(sub_i2rm_bv);
-
+make_instr_func(sub_i2rm_v);
 
 
 #endif
--- nemu/include/cpu/instr/xor.h
+++ nemu/include/cpu/instr/xor.h
@@ -2,7 +2,7 @@
 #define __INSTR_XOR_H__
 
 make_instr_func(xor_i2rm_bv);
-
+make_instr_func(xor_i2rm_v);
 
 
 #endif
