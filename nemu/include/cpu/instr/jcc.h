102b30991e929dd65b555013aae9b8080ac25528
monitor <tracer@njuics.org>
2019-10-27 16:46:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -9,4 +9,13 @@ make_instr_func(jne_short_);
 make_instr_func(jna_short_);
 make_instr_func(jg_short_);
 make_instr_func(jle_short_);
+make_instr_func(jo_near);
+make_instr_func(jno_near);
+make_instr_func(jb_near);
+make_instr_func(jae_near);
+make_instr_func(je_near);
+make_instr_func(jne_near);
+make_instr_func(jna_near);
+make_instr_func(ja_near);
+
 #endif
