3f86a2c25761e7599641b90ec13db87edbf8cdd7
monitor <tracer@njuics.org>
2019-10-27 17:21:02 +0800

    git monitor commit.

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -3,7 +3,7 @@
 instr_func opcode_entry[256] = {
     /* 0x00 - 0x03*/ add_r2rm_b,add_r2rm_v, inv, inv,
     /* 0x04 - 0x07*/ inv, add_i2a_v, inv, inv,
-    /* 0x08 - 0x0b*/ inv, or_r2rm_v, inv, inv,
+    /* 0x08 - 0x0b*/ inv, __ref_or_r2rm_v, inv, inv,
     /* 0x0c - 0x0f*/ or_i2a_b, inv, inv, opcode_2_byte,
     /* 0x10 - 0x13*/ inv, adc_r2rm_v, inv, inv,
     /* 0x14 - 0x17*/ adc_i2a_b, inv, inv, inv,
@@ -13,7 +13,7 @@ instr_func opcode_entry[256] = {
     /* 0x24 - 0x27*/ inv, inv, inv, inv,
     /* 0x28 - 0x2b*/ inv, inv, inv, inv,
     /* 0x2c - 0x2f*/ inv, inv, inv, inv,
-    /* 0x30 - 0x33*/ inv, xor_r2rm_v, inv, inv,
+    /* 0x30 - 0x33*/ inv, __ref_xor_r2rm_v, inv, inv,
     /* 0x34 - 0x37*/ inv, inv, inv, inv,
     /* 0x38 - 0x3b*/ inv, cmp_r2rm_v, inv, inv,
     /* 0x3c - 0x3f*/ inv, inv, inv, inv,
