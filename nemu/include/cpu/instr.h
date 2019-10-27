5d4701e689c93c398a79ad9c296f26b5193716b3
monitor <tracer@njuics.org>
2019-10-27 22:38:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -56,5 +56,14 @@ make_instr_func(mul_rm2a_v);
 make_instr_func(imul_rm2a_v);
 make_instr_func(div_rm2a_v);
 make_instr_func(idiv_rm2a_v);
-make_instr_func(imul_rm2r_v);
+make_instr_func(imul_rm2a_v);
+
+make_instr_func(test_i2rm_b);
+make_instr_func(not_rm_b);
+make_instr_func(neg_rm_b);
+make_instr_func(mul_rm2a_b);
+make_instr_func(imul_rm2a_b);
+make_instr_func(div_rm2a_b);
+make_instr_func(idiv_rm2a_b);
+make_instr_func(imul_rm2a_b);
 #endif
