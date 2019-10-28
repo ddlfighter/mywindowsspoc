7d1a97014bb765c59ca9f63e479361206915fa7e
tracer-pa-public <tracer@njuics.org>
2019-10-28 17:01:25 +0800

    >  nemu 181860021 fsq Linux debian 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:01:25 up  1:44,  1 user,  load average: 0.52, 0.58, 0.49 c3500c5f2bf42552ac119607e4e19857353dd8cc

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -68,15 +68,15 @@ instr_func __ref_opcode_entry[256] = {
 };
 
 /* 0x80 */
-instr_func __ref_group_1_b_entry[8] =
+instr_func group_1_b_entry[8] =
     {add_i2rm_b, or_i2rm_b, adc_i2rm_b, sbb_i2rm_b, and_i2rm_b, sub_i2rm_b, xor_i2rm_b,  __ref_cmp_i2rm_b};
 
 /* 0x81 */
-instr_func __ref_group_1_v_entry[8] =
+instr_func group_1_v_entry[8] =
     {add_i2rm_v, or_i2rm_v, adc_i2rm_v, sbb_i2rm_v, and_i2rm_v, sub_i2rm_v, xor_i2rm_v,  __ref_cmp_i2rm_v};
 
 /* 0x83 */
-instr_func __ref_group_1_bv_entry[8] =
+instr_func group_1_bv_entry[8] =
     {add_i2rm_bv,  or_i2rm_bv,  adc_i2rm_bv,  sbb_i2rm_bv,  and_i2rm_bv,  sub_i2rm_bv,  xor_i2rm_bv,   __ref_cmp_i2rm_bv};
 
 /* 0xc0 */
