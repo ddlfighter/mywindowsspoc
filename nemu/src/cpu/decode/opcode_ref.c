791f318c0320e2795a0e25fd36f48f6591b74a39
tracer-pa-public <tracer@njuics.org>
2019-10-28 17:59:33 +0800

    >  nemu 181860021 fsq Linux debian 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  17:59:33 up  2:42,  1 user,  load average: 0.51, 0.66, 0.92 972045c684e838006fcb2c6b840f75282fc17095

--- nemu/src/cpu/decode/opcode_ref.c
+++ nemu/src/cpu/decode/opcode_ref.c
@@ -108,7 +108,7 @@ instr_func group_3_b_entry[8] =
     {__ref_test_i2rm_b, inv, __ref_not_rm_b, __ref_neg_rm_b, __ref_mul_rm2a_b, __ref_imul_rm2a_b, __ref_div_rm2a_b, __ref_idiv_rm2a_b};
 
 /* 0xf7 */
-instr_func group_3_v_entry[8] =
+instr_func __ref_group_3_v_entry[8] =
     {__ref_test_i2rm_v, inv, __ref_not_rm_v, __ref_neg_rm_v, __ref_mul_rm2a_v, __ref_imul_rm2a_v, __ref_div_rm2a_v, __ref_idiv_rm2a_v};
 
 /* 0xff */
