67819e849166453ff0b63deb756cb24d7479520e
tracer-pa-public <tracer@njuics.org>
2019-10-30 02:46:46 +0800

    >  nemu 181860021 fsq Linux debian 4.19.0-6-686 #1 SMP Debian 4.19.67-2 (2019-08-28) i686 GNU/Linux  02:46:46 up  1:00,  1 user,  load average: 0.15, 0.19, 0.25 f04e1974a3bd758c2ae9edeb75a0bcc163b9b53

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -18,7 +18,7 @@ static void instr_execute_2op()
 make_instr_impl_2op(cmp,i,rm,bv);
 make_instr_impl_2op(cmp,i,rm,b);
 make_instr_impl_2op(cmp,i,rm,v);
-make_instr_impl_2op(cmp,r,rm,v);
+//make_instr_impl_2op(cmp,r,rm,v);
 make_instr_impl_2op(cmp,r,rm,b);
 make_instr_impl_2op(cmp,rm,r,b);
 make_instr_impl_2op(cmp,rm,r,v);
@@ -26,5 +26,16 @@ make_instr_impl_2op(cmp,i,a,b);
 make_instr_impl_2op(cmp,i,a,v);
 
 make_instr_func(cmp_rm2r_v){
+	OPERAND rm,r;
+	rm.data_size = 32;
+	r.data_size = 32;
+	int len = 1;
+	len += modrm_r_rm(eip+1,&r,&rm);
 
+	operand_read(&r);
+	operand_read(&rm);
+
+	alu_sub(rm.val,r.val,32);
+
+	return len;
 }
