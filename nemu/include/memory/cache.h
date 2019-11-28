ad0de9bcae4d3e16c41583a8410ae5e895694486
monitor <tracer@njuics.org>
2019-11-29 00:35:01 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -3,3 +3,10 @@ struct CacheLine{
 	int set_associaive[7];	//8-way set associative
 	int data[24];
 }
+
+init_cahce();
+
+uint32_t cache_read(paddr_t paddr,size_t len,CacheLine* cache);
+
+void cache_write(paddr_t paddr,size_t len,uint32_t data,CacheLine *cache);
+
