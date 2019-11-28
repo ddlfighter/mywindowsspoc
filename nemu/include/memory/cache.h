c74a1eabc7cb7d898b7eb2133537abb5ce1f62d8
monitor <tracer@njuics.org>
2019-11-29 01:28:02 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,7 +1,7 @@
-struct CacheLine{
-	int sign;	//valid bit
-	int set_associaive[7];	//8-way set associative
-	int data[24];
+﻿struct CacheLine{
+	int valid_bit;
+	int sign;	
+	int data[6];
 }
 
 init_cahce();
