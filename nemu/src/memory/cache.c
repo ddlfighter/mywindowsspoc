b0c8a3d32e4d5ef087f0f7a94fa9ce5c8b005755
monitor <tracer@njuics.org>
2019-11-29 15:00:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -4,11 +4,16 @@ CacheLine cl[1024];
 
 init_cache()
 {
-	for(int i=0;i<512;i++)
-		cl[i].sign = 0;
+	for(int i=0;i<1024;i++)
+		cl[i].valid_bit = 0;
 }
 
 uint_t cache_read(paddr_t paddr,size_t len,CacheLine *cache)
 {
-	int mark = paddr[31];
+	uint32_t tag = paddr>>13;		//19 bits tag
+	uint32_t grp_num = ((paddr<<19)>>19)>>6; //7 bits group number
+        uint8_t block_addr = (paddr<<26)>>26;   //6 bits block_addr
+	
+	uint32_t line_num = grp_num * 8;	//8-way set associative
+		
 }
