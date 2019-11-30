f44ad3923692961e1807fa7618cb5cf674e4904c
monitor <tracer@njuics.org>
2019-11-30 21:24:01 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -100,15 +100,8 @@ void cache_write(paddr_t paddr,size_t len,uint32_t data,struct CacheLine *cache)
 		//hit the target
 		if(cl[line_num_bg+offset].tag == tag&&cl[line_num_bg+offset].valid_bit==1)
 		{
-			if(block_addr+len<=64)	//does'n need enjambment
-			{
 				memcpy(cl[line_num_bg+offset].data+block_addr,&data,len);
-			} 
-			else
-			{
-				cache_write(paddr,64-block_addr,data,cl);
-				cache_write(paddr+64-block_addr,len+block_addr-64,data>>(8*(64-block_addr)),cl);
-			}
+	
 			break;
 		}
 	}
