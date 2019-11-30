51fce84bf598cbfe795375d93163843175784147
tracer-pa-public <tracer@njuics.org>
2019-11-30 20:59:55 +0800

    >  nemu 181860021 fsq Linux fsq 4.19.0-6-686 #1 SMP Debian 4.19.67-2+deb10u2 (2019-11-11) i686 GNU/Linux  20:59:55 up  4:41,  1 user,  load average: 0.17, 0.23, 0.21 6db00629aa930d58fe33c684d11f4c55ebc52a4e

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -49,12 +49,15 @@ uint32_t cache_read(paddr_t paddr,size_t len,struct CacheLine *cache)
 			break;
 		}
 	}
-	
-
 	if(!if_hit&&block_addr!=0)
 	{
-		printf("The  read hit line is in:%u\nThe read offset is:%u\nThe length is:%u\n",line_num_bg,block_addr,len);
+				printf("The  read hit line is in:%u\nThe read offset is:%u\nThe length is:%u\n",line_num_bg,block_addr,len);
 		printf("if_hit:%d\n",if_hit);
+	}
+
+	if(!if_hit)
+	{
+
 		memcpy(&ret,hw_mem+paddr,len);
 		bool exist = false;
 		int h = 0;
