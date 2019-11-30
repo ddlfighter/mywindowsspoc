1331a20725d7057e32741ab9359a3c8ad0bdddfe
tracer-pa-public <tracer@njuics.org>
2019-11-30 19:28:18 +0800

    >  nemu 181860021 fsq Linux fsq 4.19.0-6-686 #1 SMP Debian 4.19.67-2+deb10u2 (2019-11-11) i686 GNU/Linux  19:28:18 up  3:09,  1 user,  load average: 0.52, 0.25, 0.13 6189f7c3fc1a385680fb0cecc5440110eb0547c5

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -85,7 +85,8 @@ void cache_write(paddr_t paddr,size_t len,uint32_t data,struct CacheLine *cache)
 	uint32_t tag = paddr>>13;		//19 bits tag
 	uint32_t grp_num = ((paddr<<19)>>19)>>6; //7 bits group number
     uint32_t block_addr = (paddr<<26)>>26;   //6 bits block_addr
-
+	uint32_t line_num_bg = grp_num * 8;	//8-way set associative
+	
 	int offset=0;
 	bool flag1 = false;
 	
