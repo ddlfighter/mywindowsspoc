93ab5060fa2a797fa66f891a64f4becf0b5d45ee
tracer-pa-public <tracer@njuics.org>
2019-12-03 20:03:31 +0800

    >  nemu 181860021 fsq Linux fsq 4.19.0-6-686 #1 SMP Debian 4.19.67-2+deb10u2 (2019-11-11) i686 GNU/Linux  20:03:31 up  5:24,  1 user,  load average: 0.39, 0.17, 0.06 4b3dddff1a9ee6e534059b972f07a8932e0d514e

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -3,18 +3,13 @@
 make_instr_func(lgdt)
 {
     int len = 1;
-    OPERAND rel;
-    len += modrm_rm(eip+1,&rel);
+    OPERAND rm;
+    rm.data_size = data_size;
+    len+=modrm_rm(eip+1,&rm);
 
-    rel.data_size = 16;
-    operand_read(&rel);
-    cpu.gdtr.limit = rel.val;
-    //get the limit value
-
-    rel.addr += 2;
-    operand_read(&rel);
-    cpu.gdtr.base = rel.val;
-    print_asm_1("lgdt","",len,&rel);
+    cpu.gdtr.limit = paddr_read(rm.addr,2);
+    cpu.gdtr.base = paddr_read(rm.addr+2,4);
+    print_asm_1("lgdt","",5,&rm);
 
     return len;
 }
