f8b8ceb0e7b9c36dfbefa9f2048304fbcd0069ee
tracer-pa-public <tracer@njuics.org>
2019-12-10 18:48:12 +0800

    >  clean 181860021 fsq Linux fsq 4.19.0-6-686 #1 SMP Debian 4.19.67-2+deb10u2 (2019-11-11) i686 GNU/Linux  18:48:12 up  1:58,  1 user,  load average: 0.24, 0.43, 0.40 4666913abf6af71822b1c152ec9bad0342a20466

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,8 +5,15 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-	printf("\nPlease implement page_translate()\n");
-	assert(0);
+	uint32_t dir = (laddr>>22)&0x3ff;
+	uint32_t page = (laddr>>12)&0x3ff;
+	uint32_t offset = laddr & 0xfff;
+	PDE * pdir = (PDE *)(hw_mem+(cpu.cr3.pdtr<<12)+(dir<<2));
+	PTE * ptable = (PTE *)(hw_mem+(pdir->page_frame<<12)+(page<<2));
+	assert(pdir->present==1);
+	assert(ptable->present==1);
+	paddr_t ret = (ptable->page_frame<<12)+offset;
+	return ret;
 #else
 	return tlb_read(laddr) | (laddr & PAGE_MASK);
 	;
