a66b744560175536773ff16841e679bd4ae289f4
tracer-pa-public <tracer@njuics.org>
2020-01-15 18:47:40 +0800

    >  nemu 181860021 fsq Linux fsq 4.19.0-6-686 #1 SMP Debian 4.19.67-2+deb10u2 (2019-11-11) i686 GNU/Linux  18:47:39 up  4:13,  1 user,  load average: 0.47, 0.53, 0.37 e9dcb09b241f2b71e6e3c2afbf08394150b6fe7f

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -8,7 +8,8 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t dir = (laddr>>22)&0x3ff;
 	uint32_t page = (laddr>>12)&0x3ff;
 	uint32_t offset = laddr & 0xfff;
-	printf("%ld    %ld\n",&cpu.cr3.pdtr,&dir);
+	uint32_t tmp = cpu.cr3.pdtr;
+	printf("%ld    %ld\n",&tmp,&dir);
 	PDE * pdir = (PDE *)(hw_mem+(cpu.cr3.pdtr<<12)+(dir<<2));
 	PTE * ptable = (PTE *)(hw_mem+(pdir->page_frame<<12)+(page<<2));
 	assert(pdir->present==1);
