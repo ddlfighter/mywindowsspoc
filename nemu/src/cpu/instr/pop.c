d31ab90c938ff6366f5da6f3e7adb730b74183e6
monitor <tracer@njuics.org>
2020-01-15 23:54:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -17,3 +17,49 @@ make_instr_func(pop_r_v){
     cpu.esp += 4;
     return 1;
 }
+
+make_instr_func(popa) {
+	print_asm_0("popa", "", 1);
+	opr_src.data_size=32;
+	opr_src.type=OPR_MEM;
+	opr_src.sreg=SREG_DS;
+
+	opr_src.addr=cpu.esp;
+	operand_read(&opr_src);
+	cpu.edi=opr_src.val;
+	cpu.esp+=32/8;
+
+	opr_src.addr=cpu.esp;
+	operand_read(&opr_src);
+	cpu.esi=opr_src.val;
+	cpu.esp+=32/8;
+	
+	opr_src.addr=cpu.esp;
+	operand_read(&opr_src);
+	cpu.ebp=opr_src.val;
+	cpu.esp+=32/8;
+	
+	cpu.esp+=32/8;
+	
+	opr_src.addr=cpu.esp;
+	operand_read(&opr_src);
+	cpu.ebx=opr_src.val;
+	cpu.esp+=32/8;
+
+	opr_src.addr=cpu.esp;
+	operand_read(&opr_src);
+	cpu.edx=opr_src.val;
+	cpu.esp+=32/8;
+
+	opr_src.addr=cpu.esp;
+	operand_read(&opr_src);
+	cpu.ecx=opr_src.val;
+	cpu.esp+=32/8;
+
+	opr_src.addr=cpu.esp;
+	operand_read(&opr_src);
+	cpu.eax=opr_src.val;
+	cpu.esp+=32/8;
+
+	return 1;
+}
