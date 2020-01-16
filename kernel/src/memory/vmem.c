dff8986efc3471b2e206a0cf3b746f70c6d78554
monitor <tracer@njuics.org>
2020-01-16 13:06:01 +0800

    git monitor commit.

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -4,19 +4,19 @@
 
 #define VMEM_ADDR 0xa0000
 #define SCR_SIZE (320 * 200)
-#define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE) // number of page tables to cover the vmem
+#define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE)  // number of page tables to cover the vmem
 
-PDE *get_updir();
+PDE* get_updir();
 PTE table[1024] align_to_page;
 
-void create_video_mapping()
-{
-
+void create_video_mapping() {
+	
 	/* TODO: create an identical mapping from virtual memory area
 	 * [0xa0000, 0xa0000 + SCR_SIZE) to physical memeory area
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
+	//[0xa0000, 0xafa00)
 	PDE *pdir = (PDE *)va_to_pa(get_updir());
 	PTE *ptable = (PTE *)va_to_pa(table);
 	pdir[0].val = make_pde(ptable); 
@@ -28,27 +28,22 @@ void create_video_mapping()
 	//panic("please implement me");
 }
 
-void video_mapping_write_test()
-{
+void video_mapping_write_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
-	for (i = 0; i < SCR_SIZE / 4; i++)
-	{
+	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		buf[i] = i;
 	}
 }
 
-void video_mapping_read_test()
-{
+void video_mapping_read_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
-	for (i = 0; i < SCR_SIZE / 4; i++)
-	{
+	for(i = 0; i < SCR_SIZE / 4; i ++) {
 		assert(buf[i] == i);
 	}
 }
 
-void video_mapping_clear()
-{
+void video_mapping_clear() {
 	memset((void *)VMEM_ADDR, 0, SCR_SIZE);
 }
