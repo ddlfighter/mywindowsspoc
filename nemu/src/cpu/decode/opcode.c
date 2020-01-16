9affb0692a91b554987ea0b82e5acedcd90aab8f
tracer-pa-public <tracer@njuics.org>
2020-01-16 14:11:34 +0800

    >  clean 181860021 fsq Linux fsq 4.19.0-6-686 #1 SMP Debian 4.19.67-2+deb10u2 (2019-11-11) i686 GNU/Linux  14:11:34 up  2:21,  1 user,  load average: 0.35, 0.33, 0.84 d15a74553206155b63a5bb55696385c39753b520

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -1,6 +1,6 @@
 #include "cpu/instr.h"
 
-instr_func opcode_entry[256] = {
+instr_func __ref_opcode_entry[256] = {
     /* 0x00 - 0x03*/ add_r2rm_b,add_r2rm_v, add_rm2r_b, add_rm2r_v,
     /* 0x04 - 0x07*/ add_i2a_b, add_i2a_v, inv, inv,
     /* 0x08 - 0x0b*/ inv, or_r2rm_v, or_rm2r_b, inv,
